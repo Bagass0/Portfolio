8a816f5e796ecabe3b7b97f70ac57f3cf721939b
Hugo <80489889+Bagass0@users.noreply.github.com>
2025-05-22 15:52:27 +0200

    first_commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Bienvenue sur mon Portfolio</h1>
+      <p>Ce site est déployé automatiquement via Git.</p>
+    </div>
+  )
+}
+
+export default App
