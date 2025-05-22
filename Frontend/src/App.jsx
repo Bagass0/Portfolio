c4ad6f5e238e2eb6b92fe4438d17802681ac8e34
Hugo <80489889+Bagass0@users.noreply.github.com>
2025-05-22 23:48:19 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,9 @@ import './App.css'
 function App() {
   return (
     <div className="container">
-      <h1>Bienvenue sur mon Portfolio Bagasso</h1>
+      <h1>Bienvenue sur mon Portfolio</h1>
       <p>Ce site est déployé automatiquement via Git.</p>
+      <p>Signed by Bagasso</p>
     </div>
   )
 }
