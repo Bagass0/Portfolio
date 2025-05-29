d8221512b43cd83407d4cbed8d4b12b769d260f4
Hugo <80489889+Bagass0@users.noreply.github.com>
2025-05-29 03:40:18 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './Composants/Navbar';
+
 import Footer from './Composants/Footer';
 import Home from './Pages/Home';
 import Projects from './Pages/Projects';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import NotFound from './Pages/NotFound';
+import Navbar from './Composants/Navbar';
 
 function App() {
   return (
