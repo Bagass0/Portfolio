beec1f1786e3db562637ff501efbb3489cab34f7
Hugo <80489889+Bagass0@users.noreply.github.com>
2025-06-10 16:54:48 +0200

    Update Contact & CSS

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -9,24 +9,26 @@ const Contact = () => (
     <p className="contact-intro">
       Discutons ensemble ! Pour toute collaboration, question ou opportunité, contactez-moi via l’un des moyens ci-dessous.
     </p>
-    <div className="contact-box">
+    <div className="contact-grid">
       <a
         href="mailto:hugo.b_pereira@outlook.com"
-        className="contact-link"
+        className="contact-card"
         aria-label="Mail"
       >
-        <FontAwesomeIcon icon={faEnvelope} />
-        hugo.b_pereira@outlook.com
+        <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+        <div className="contact-type">Email</div>
+        <div className="contact-value">hugo.b_pereira@outlook.com</div>
       </a>
       <a
         href="https://linkedin.com/in/hugo-barbosa-pereira"
         target="_blank"
         rel="noopener noreferrer"
-        className="contact-link"
+        className="contact-card"
         aria-label="LinkedIn"
       >
-        <FontAwesomeIcon icon={faLinkedin} />
-        linkedin.com/in/hugo-barbosa-pereira
+        <FontAwesomeIcon icon={faLinkedin} className="contact-icon" />
+        <div className="contact-type">LinkedIn</div>
+        <div className="contact-value">BARBOSA Hugo</div>
       </a>
     </div>
   </div>
